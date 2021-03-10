847e9204984fc58725ec08e59b7f868f6e69f5af
sisirrag <mca549@rajagiri.edu>
2021-03-10 16:03:19 +0530

    m

--- index.php
+++ index.php
@@ -116,7 +116,7 @@
             </table>
         </div>
     <div class="card" style="width: 18rem;">
-      <img src="....jpg" class="card-img-top" alt="...">
+      <img src="https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
